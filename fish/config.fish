4358a01cfb3123787b09c970288e69513b9d7ab2
Jessica Bellon <jessica.bellon@rewardStyle.com>
2020-09-10 09:46:21 -0500

    add thefuck

--- fish/config.fish
+++ fish/config.fish
@@ -28,3 +28,7 @@ fish_vi_key_bindings
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
+thefuck --alias | source 
+
+set PATH $PATH /Users/jessicabellon/.local/bin
